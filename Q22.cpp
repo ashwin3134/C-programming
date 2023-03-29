3032a884458ded4bcd8060ca0a6b5478ee20fb9a
ashwin3134 <68997869+ashwin3134@users.noreply.github.com>
2023-03-29 18:59:55 +0530

    Update Q22.cpp

--- Q22.cpp
+++ Q22.cpp
@@ -3,15 +3,11 @@
 int main() {
     char str[100];
     int len = 0, i;
-
     printf("Enter a string: ");
     fgets(str, 100, stdin);
-
     for (i = 0; str[i] != '\0'; i++) {
         len++;
     }
-
     printf("The length of the string is %d\n", len);
-
     return 0;
 }
