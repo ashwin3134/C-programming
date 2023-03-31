e191e95ad513f950e7bcb1a8a296d9443638cccd
ashwin3134 <68997869+ashwin3134@users.noreply.github.com>
2023-03-31 10:14:59 +0530

    Add files via upload

---
+++ D3 Q6.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int isPrime(int num);           
+int main()
+{
+    int start, end, i;
+    printf("Enter the starting and ending numbers of the range: ");
+    scanf("%d %d", &start, &end);
+    printf("Prime numbers between %d and %d are: ", start, end);
+    for (i = start; i <= end; ++i) {
+        if (isPrime(i))
+            printf("%d ", i);
+    }
+    return 0;
+}
+int isPrime(int num)
+{
+    int i;
+    if (num == 1)
+        return 0;
+    for (i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
